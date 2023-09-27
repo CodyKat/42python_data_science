36628045a86fb5c662ca135e8fae6e1422487f48
jaemjeon <piepie1346@gmail.com>
2023-09-28 08:10:44 +0900

    first commit

---
+++ 00/ex01/format_ft_time.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+
+old = datetime(1970, 1, 1, 0, 0, 0)
+new = datetime.now()
+
+seconds_past = (new - old).total_seconds()
+
+print("Seconds since January 1, 1970: {0:,.4f} or {1:2.2e} in scientific notation".format(seconds_past, seconds_past))
+print("{0:s} {1:s} {2:4d}".format(new.strftime("%b"), new.strftime("%m"), new.year))
---
+++ 00/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+def NULL_not_found(object: any) -> int:
+	typeinfo = type(object)
+	typename = typeinfo.__name__
+	if typename == "NoneType":
+		print("Nothing: {} {}".format(object, typeinfo))
+	elif typename == "int":
+		print("Zero: {} {}".format(object, typeinfo))
+	elif typename == "float":
+		print("Cheese: {} {}".format(object, typeinfo))
+	elif typename == "str" and object == '':
+		print("Empty: {} {}".format(object, typeinfo))
+	elif typename == "bool":
+		print("Fake: {} {}".format(object, typeinfo))
+	else:
+		print("Type not Found")
+	return 1
+
