d85342acf0631051247391b98bd4bdbba255b596
jaemjeon <piepie1346@gmail.com>
2023-09-28 23:22:19 +0900

    ex02 수정

--- 00/ex02/find_ft_type.py
+++ 00/ex02/find_ft_type.py
@@ -1,10 +1,11 @@
 def all_thing_is_obj(object: any) -> int:
     typename = type(object).__name__
     typename = typename.capitalize()
-    if (typename == "Str"):
-        print("{} is in the kitchen : {}".format(object, type(object)))
-    elif (typename == "Int"):
-        print("Type not found")
-    else:
+    if typename == 'List' or typename == 'Tuple' or \
+        typename == 'Set' or typename == 'Dict':
         print("{} : {}".format(typename, type(object)))
+    elif typename == "Str":
+        print("{} is in the kitchen : {}".format(object, type(object)))
+    else:
+        print("Type not found")
     return 42
