c1f0bfd89545368a591290a8b5a7b57d45bb48ce
CodyKat <piepie1346@gmail.com>
2023-09-28 10:25:46 +0900

    ex03 ex04

---
+++ 00/ex04/whatis.py
@@ -0,0 +1,21 @@
+import sys
+
+def isdigit(string : str) -> bool:
+    for i in string:
+        if i.isdigit() == False:
+            return False
+    return True
+
+if len(sys.argv) == 1:
+    sys.exit()
+elif len(sys.argv) > 2:
+    print("AssertionError: more than one argument is provided")
+else:
+    if isdigit(sys.argv[1]):
+        number_arg = int(sys.argv[1])
+        if number_arg % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    else:
+        print("AssertionError: argument is not an integer")
