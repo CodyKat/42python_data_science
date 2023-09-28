7a2ba71e7e02af621424e8fd319c66b457d37607
jaemjeon <piepie1346@gmail.com>
2023-09-28 23:17:54 +0900

    ex01 day 출력

--- 00/ex01/format_ft_time.py
+++ 00/ex01/format_ft_time.py
@@ -7,7 +7,7 @@ def main():
     seconds_past = (new - old).total_seconds()
 
     print("Seconds since January 1, 1970: {0:,.4f} or {1:2.2e} in scientific notation".format(seconds_past, seconds_past))
-    print("{0:s} {1:s} {2:4d}".format(new.strftime("%b"), new.strftime("%m"), new.year))
+    print("{0:s} {1:s} {2:4d}".format(new.strftime("%b"), new.strftime("%d"), new.year))
 
 if __name__ == "__main__":
     main()
