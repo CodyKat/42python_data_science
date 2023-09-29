70149dcb64c37e185ce20e77e7d77b22f73b5d85
CodyKat <piepie1346@gmail.com>
2023-09-29 13:09:49 +0900

    finish 01 ex02

---
+++ 01/ex02/load_image.py
@@ -0,0 +1,17 @@
+import PIL
+from PIL import Image
+import numpy as np
+from numpy import array
+
+
+def ft_load(path: str) -> array:
+    image : PIL.JpegImagePlugin.JpegImageFile
+    try:
+        image = Image.open(path)
+    except FileNotFoundError:
+        raise AssertionError('image file not found')
+    print(type(image))
+
+    print(np.shape(image))
+    np_img = np.array(image)
+    return np_img
