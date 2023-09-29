91e23d0f9a69b484ce0c5695ea3c0e36b10400c8
CodyKat <piepie1346@gmail.com>
2023-09-29 12:59:38 +0900

    finish 01 ex01

--- 01/ex01/array2D.py
+++ 01/ex01/array2D.py
@@ -1,2 +1,15 @@
+import numpy as np
+
+
 def slice_me(family: list, start: int, end: int) -> list:
-	assert family is isinstance(list)
+	assert isinstance(family, list), 'family must be list'
+	assert isinstance(start, int), 'start index must be int'
+	assert isinstance(end, int), 'end index must be int'
+
+	print('My shap is : ', np.shape(family))
+	new_shape = family[start:end]
+	print('My new shape is : ', np.shape(new_shape))
+	return new_shape
+
+
+slice_me.__doc__ = "just slice your matrix from start to end"
