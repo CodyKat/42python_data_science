b7458e4055e629cb43e4cb6d33e70b40e5bb3e6f
jaemjeon <piepie1346@gmail.com>
2023-09-30 00:00:01 +0900

    finish 01 ex03

--- 01/ex03/zoom.py
+++ 01/ex03/zoom.py
@@ -1,18 +1,20 @@
-from numpy import array
+import cv2
 import numpy as np
+
+# assuming ft_load is defined in load_image and loads an image
 from load_image import ft_load
-from PIL import Image
 
-image : array = ft_load('animal.jpeg')
-print(np.shape(image))
-imagePIL = Image.fromarray(image)
-image_resized = imagePIL.crop((450,100,850,500))
-image2 = np.array(image_resized)
-gray_image = np.mean(image2, axis=2, dtype=int)
-to_print = Image.fromarray(gray_image)
-
-
-print("New shape after slicing: (400, 400, 1) or (400, 400)")
-print(to_print)
-
-to_print.show()
+img: np.array = ft_load('animal.jpeg')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Assume we want to zoom into the center of the image
+start_row = 100
+start_col = 450
+cropped_image_2d = gray_img[start_row:start_row + 400, start_col:start_col + 400]
+print('2d shape : ', cropped_image_2d.shape)
+cropped_image_3d = np.expand_dims(cropped_image_2d, axis=-1)
+print('3d shape : ', cropped_image_3d.shape)
+print(cropped_image_3d)
+# use OpenCV to show the zoomed image
+cv2.imshow('Zoomed Image', cropped_image_3d)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
