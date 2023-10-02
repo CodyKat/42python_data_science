d69f72206ecd55da2480d196c9560b40ded4ac1e
CodyKat <piepie1346@gmail.com>
2023-10-02 10:24:17 +0900

    doing 02 ex02

--- 02/ex02/aff_pop.py
+++ 02/ex02/aff_pop.py
@@ -1,10 +1,22 @@
+from matplotlib import pyplot as plt
 from load_csv import load
 
 
 def main():
     """"""
     population_total = load('population_total.csv')
-    row = population_total[population_total['country'] == 'Souch Korea']
-
+    countrys = population_total['country']
+    selected_row = population_total.loc[
+        (population_total['country'] == 'South Korea') |
+        (population_total['country'] == 'France')
+        ]
+    selected_row = selected_row.T
+    selected_row = selected_row.loc[selected_row['country']]
+    print(selected_row)
+    # selected_row = selected_row.drop(index=1)
+    # print(selected_row)
+    # selected_row.plot()
+    # plt.show()
+    # two_countries = population_total.loc()
 if __name__ == "__main__":
     main()
