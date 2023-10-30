f75d609f4ccf94b1ab3ac6e9a958d3342a387fc7
CodyKat <piepie1346@gmail.com>
2023-10-30 10:08:25 +0900

    03 ex00 docs

--- 03/ex00/S1E9.py
+++ 03/ex00/S1E9.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Character(ABC):
-	"""Your docstring for Class"""
+	"""Character has frist_name, is_alive attribute"""
 	@abstractmethod
 	def __init__(self, first_name=None, is_alive=True):
 		self.first_name = first_name
@@ -9,9 +9,11 @@ class Character(ABC):
 
 
 class Stark(Character):
-	"""Your docstring for Class"""
+	"""Stark is derived from Character"""
 	def __init__(self, first_name=None, is_alive=True):
+		"""constructor of Stark"""
 		super().__init__(first_name, is_alive)
 
 	def die(self):
+		"""make me die"""
 		self.is_alive = False
