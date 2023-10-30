2f09cefdf04b769267e85fe14bfa12cd7995c546
CodyKat <piepie1346@gmail.com>
2023-10-30 13:22:16 +0900

    finish 03 ex01

---
+++ 03/ex01/S1E7.py
@@ -0,0 +1,52 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name=None, is_alive=True):
+        """constructor of Stark"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = 'Baratheon'
+        self.eyes = 'brown'
+        self.hairs = 'dark'
+    
+    def __str__(self) -> str:
+        return 'Baratheon'
+    
+    def __repr__(self) -> str:
+        return "Vector: ('{}', '{}', '{}')".format(self.family_name, self.eyes, self.hairs)
+
+    @classmethod
+    def create_baratheon(cls, first_name, is_alive):
+        new_baratheon = Baratheon(first_name, is_alive)
+        return new_baratheon 
+
+    def die(self):
+        """make me die"""
+        self.is_alive = False
+
+
+class Lannister(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name=None, is_alive=True):
+        """constructor of Stark"""
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = 'Lannister'
+        self.eyes = 'brown'
+        self.hairs = 'dark'
+    
+    def __str__(self) -> str:
+        return 'Lannister'
+    
+    def __repr__(self) -> str:
+        return 'Vector: ({}, {}, {})'.format(self.family_name, self.eyes, self.hairs)
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive):
+        new_lannister = Lannister(first_name, is_alive)
+        return new_lannister 
+
+    def die(self):
+        """make me die"""
+        self.is_alive = False
