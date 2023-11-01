1e21ccae9cf3acc4c5b23ac546a7e10d9588ba27
CodyKat <piepie1346@gmail.com>
2023-11-01 18:29:16 +0900

    add comment 04 ex02

--- 04/ex02/callLimit.py
+++ 04/ex02/callLimit.py
@@ -1,7 +1,10 @@
 def callLimit(limit: int):
+    """limit function call up to limit value"""
     count = 0
     def callLimiter(function):
+        """wapper function process with function"""
         def limit_function(*args: any, **kwds: any):
+            """actual action"""
             nonlocal count
             if count == limit:
                 print('Error: <function {} at {}> call too many times'.format(function.__name__, hex(id(function))))
